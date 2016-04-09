03699db1742326116fea8ba7665944545d367ca0
Moises Silveira <msilveira.ti@gmail.com>
2016-04-09 00:02:33 -0300

    Enable again bcrypt from password

--- app/models/usuario.js
+++ app/models/usuario.js
@@ -9,12 +9,12 @@ module.exports = function() {
 
     // methods ======================
     schema.methods.generateHash = function(password) {
-        return password; //bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
     };
 
     // checking if password is valid
     schema.methods.validPassword = function(password) {
-        return true;//password === this.password;//bcrypt.compareSync(password, this.password);
+        return bcrypt.compareSync(password, this.password);
     };
 
     return mongoose.model('Usuario', schema);
